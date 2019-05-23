e0616fd21fae12235836e739d1eea1ca9b00034d
bebbo <bebbo@bejy.net>
2019-05-23 20:20:14 +0200

    refs #64 #67 #82 - undo some changes - superfluous and bogus

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -3985,19 +3985,6 @@ optimize_bit_field_compare (location_t loc, enum tree_code code,
       if (nbitpos < 0)
 	return 0;
 
-#ifdef TARGET_AMIGA
-      /* use the real bit pattern without and mask, to use bfexts/bfextu */
-      lhs = make_bit_field_ref (loc, linner, lhs, unsigned_type,
-				    lbitsize, obitpos, 1, lreversep);
-
-      if (TREE_CODE(lhs) == NOP_EXPR)
-        TREE_TYPE(TREE_OPERAND(lhs, 0)) = lang_hooks.types.type_for_mode (SImode, lunsignedp);
-      else
-        TREE_TYPE(lhs) = lang_hooks.types.type_for_mode (SImode, lunsignedp);
-
-      return fold_build2_loc (loc, code, compare_type, lhs, rhs);
-
-#else
       /* If not comparing with constant, just rework the comparison
 	 and return.  */
       tree t1 = make_bit_field_ref (loc, linner, lhs, unsigned_type,
@@ -4007,7 +3994,6 @@ optimize_bit_field_compare (location_t loc, enum tree_code code,
 				    nbitsize, nbitpos, 1, rreversep);
       t2 = fold_build2_loc (loc, BIT_AND_EXPR, unsigned_type, t2, mask);
       return fold_build2_loc (loc, code, compare_type, t1, t2);
-#endif
     }
 
   /* Otherwise, we are handling the constant case.  See if the constant is too

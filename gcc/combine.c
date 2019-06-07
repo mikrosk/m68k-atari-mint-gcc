487bcf170dcd92eb1d37cbf45c69aade93dc0df1
bebbo <bebbo@bejy.net>
2019-06-07 16:59:30 +0200

    refs #97: disallow combination of const pic ref' into minus as subtrahend

--- gcc/combine.c
+++ gcc/combine.c
@@ -5357,8 +5357,16 @@ subst (rtx x, rtx from, rtx to, int in_dest, int in_cond, int unique_copy)
 		  && (((code == SUBREG || code == ZERO_EXTRACT)
 		       && REG_P (new_rtx))
 		      || code == STRICT_LOW_PART))
-		;
-
+		{
+		  // intentionally empty
+		}
+#ifdef TARGET_AMIGA
+	      /** do not subst pic_refs into minus */
+	      else if (GET_CODE(x) == MINUS && amiga_is_const_pic_ref(to))
+		{
+		  // intentionally empty
+		}
+#endif
 	      else if (COMBINE_RTX_EQUAL_P (XEXP (x, i), from))
 		{
 		  /* In general, don't install a subreg involving two

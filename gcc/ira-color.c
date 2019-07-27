2ad0993f5bae954b90d8b1a3a5c2b4bd2a9c5b06
bebbo <bebbo@bejy.net>
2019-07-27 22:30:20 +0200

    only apply it to PSEUDO regs

--- gcc/ira-color.c
+++ gcc/ira-color.c
@@ -4153,8 +4153,8 @@ remove_superfluous_stack_vars ()
 		    {
 		      /* TODO: switch to costs. */
 		      rtx x = XEXP(src, 0);
-		      if ((REG_P(x) && REGNO(x) < 13)
-			  || (GET_CODE(x) == PLUS && REG_P(XEXP(x, 0)) && REGNO(XEXP(x, 0)) < 13))
+		      if ((REG_P(x) && REGNO(x) >= FIRST_PSEUDO_REGISTER)
+			  || (GET_CODE(x) == PLUS && REG_P(XEXP(x, 0)) && REGNO(XEXP(x, 0)) >= FIRST_PSEUDO_REGISTER))
 			reg_equiv_memory_loc (a->regno) = src;
 		    }
 		  break;

4baa2a298f42587ead02d9aa91e1793c470487af
bebbo <bebbo@bejy.net>
2021-02-22 12:51:54 +0100

    another cost model update...

--- gcc/config/m68k/m68k_68080_costs.c
+++ gcc/config/m68k/m68k_68080_costs.c
@@ -21,9 +21,9 @@ selftest_m68k_68080_costs (void)
   rtx d1 = gen_rtx_REG (SImode, 1);
   rtx a0 = gen_rtx_REG (SImode, 8);
 
-  rtx c120 = gen_rtx_CONST_INT(VOIDmode, 120);
+  rtx c120 = gen_rtx_CONST_INT (VOIDmode, 120);
 
-  rtx mem_120ia0 = gen_rtx_MEM(SImode, gen_rtx_PLUS(SImode, a0, c120));
+  rtx mem_120ia0 = gen_rtx_MEM (SImode, gen_rtx_PLUS(SImode, a0, c120));
 
   rtx movel_d0_d1 = gen_rtx_SET(d1, d0);
   TEST(movel_d0_d1, 4);
@@ -56,12 +56,104 @@ m68k_68080_costs (rtx x, machine_mode mode, int outer_code, int opno,
   int code = GET_CODE(x);
   int total2 = 0;
   *total = 0;
+
+  if (outer_code == ADDRESS)
+    {
+      if (GET_CODE (x) == CONST)
+	x = XEXP(x, 0);
+      if (REG_P(x) || GET_CODE(x) == POST_INC || GET_CODE(x) == PRE_DEC)
+	{
+	  *total = 0; // (ax), (ax)+, -(ax)
+	  return true;
+	}
+      if (GET_CODE(x) == SYMBOL_REF || GET_CODE(x) == LABEL_REF || CONST_INT_P(x))
+	{
+	  *total = 2;
+	  return true;
+	}
+      if (GET_CODE(x) == MULT)
+	{
+	  *total = 3;
+	  return true;
+	}
+
+      if (GET_CODE(x) == PLUS)
+	{
+	  rtx b = XEXP(x, 0);
+	  if (GET_CODE(b) == CONST)
+	    b = XEXP(b, 0);
+	  rtx c = XEXP(x, 1);
+	  if (GET_CODE(c) == CONST)
+	    c = XEXP(c, 0);
+
+	  if ((GET_CODE(b) == SYMBOL_REF || GET_CODE(b) == LABEL_REF  || CONST_INT_P(b))
+	      && CONST_INT_P(c))
+	    {
+	      *total = 2; // sym+n
+	      return true;
+	    }
+
+	  if (REG_P(b) && GET_CODE(c) == CONST_INT && INTVAL(c) >= -32768
+	      && INTVAL(c) <= 37267)
+	    {
+	      *total = 1; // 16(An),Dn
+	      return true;
+	    }
+	  if (REG_P(b) || GET_CODE(b) == MULT || GET_CODE(b) == ASHIFT)
+	    {
+	      if (REG_P(c))
+		{
+		  *total = 3; // (An,Xn*S)
+		  return true;
+		}
+	      if (REG_P(c) || GET_CODE(c) == SYMBOL_REF
+		  || GET_CODE(c) == CONST_INT)
+		{
+		  *total = 5; // (32,Xn*S)
+		  return true;
+		}
+	    }
+	  if (GET_CODE(b) == PLUS)
+	    {
+	      rtx b0 = XEXP(b, 0);
+	      rtx b1 = XEXP(b, 1);
+	      if (REG_P(b0) || GET_CODE(b0) == MULT || GET_CODE(b0) == ASHIFT)
+		{
+		  if (GET_CODE(c) == CONST_INT)
+		    {
+		      *total = INTVAL(c) >= -8 && INTVAL(c) <= 7 ? 3 // 8(An,Xn*S)
+			  :
+			       INTVAL(c) >= -32768 && INTVAL(c) <= 32767 ? 4 // (16,An,Xn*S)
+				   : 5; // (32,An,Xn*S)
+		      return true;
+		    }
+		  if (GET_CODE(c) == SYMBOL_REF || GET_CODE(c) == LABEL_REF || CONST_INT_P(c))
+		    {
+		      *total = 5;
+		      return true;
+		    }
+		}
+	      if (GET_CODE(b0) == SYMBOL_REF && CONST_INT_P(b1))
+		{
+		  *total = 5;
+		  return true;
+		}
+	    }
+	  if (GET_CODE(c) == PLUS)
+	    {
+	      *total = 5;
+	      return true;
+	    }
+	}
+      *total = 35;
+      return true;
+    }
+
   switch (code)
     {
     case CC0:
     case PC:
     case REG:
-      *total = 2;
       return true;
 
     case CONST_INT:
@@ -70,23 +162,23 @@ m68k_68080_costs (rtx x, machine_mode mode, int outer_code, int opno,
 
 	// moveq
 	if (outer_code == SET && iv >= -0x100 && iv <= 0xff)
-	  *total = 2;
+	  ;
 	else
 	// addq
 	if ((outer_code == PLUS || outer_code == MINUS) && iv >= -0x8
 	    && iv <= 0x7)
-	  *total = 2;
+	  ;
 	else
 	// .b .w *iw.l
 	if (GET_MODE_SIZE(mode) < 4 && iv >= -32768 && iv <= 32767)
-	  *total = 3;
+	  *total = 1;
 	else
-	  *total = 4;
+	  *total = 2;
       }
       return true;
 
     case CONST_DOUBLE:
-      *total = mode == SFmode ? 6 : mode == DFmode ? 8 : 10;
+      *total = mode == SFmode ? 2 : mode == DFmode ? 4 : 6;
       return true;
 
     case FLOAT:
@@ -109,8 +201,7 @@ m68k_68080_costs (rtx x, machine_mode mode, int outer_code, int opno,
     case UDIV:
     case MOD:
     case DIV:
-      total2 = 30; // always in a parallel with DIV and MOD -> half costs
-      // with regs, 2 are added by the caller and 2 by the code below
+      total2 = 60; // always in a parallel with DIV and MOD -> half costs
       goto OpCost;
 
     case ASHIFT:
@@ -130,7 +221,7 @@ m68k_68080_costs (rtx x, machine_mode mode, int outer_code, int opno,
 	total2 += *total;
 
 	// add cost for 2nd
-	m68k_68080_costs (a, mode, code, 0, total, speed);
+	m68k_68080_costs (b, mode, code, 0, total, speed);
 	*total += total2;
 	return true;
       }
@@ -151,116 +242,92 @@ m68k_68080_costs (rtx x, machine_mode mode, int outer_code, int opno,
 
     case CALL:
       {
-	// does not matter
-	*total = 16;
+	bool r = m68k_68080_costs (XEXP(x, 0), mode, ADDRESS, 0, total, speed);
+	*total += 7;
 	return true;
       }
 
     case MEM:
       {
-	rtx a = XEXP(x, 0);
-	if (REG_P(a) || GET_CODE(a) == POST_INC
-	    || GET_CODE(a) == PRE_DEC)
-	  {
-	    *total = 5; // (ax), (ax)+, -(ax)
-	    return true;
-	  }
-	if (GET_CODE(a) == SYMBOL_REF || GET_CODE(a) == LABEL_REF)
-	  {
-	    *total = 9;
-	    return true;
-	  }
-	if (GET_CODE(a) == PLUS)
-	  {
-	    rtx b = XEXP(a, 0);
-	    rtx c = XEXP(a, 1);
-	    if (REG_P(b) && GET_CODE(c) == CONST_INT && INTVAL(c) >= -32768
-		&& INTVAL(c) <= 37267)
-	      {
-		*total = 6; // 16(An),Dn
-		return true;
-	      }
-	    if (REG_P(b) || GET_CODE(b) == MULT || GET_CODE(b) == ASHIFT)
-	      {
-		if (REG_P(c))
-		  {
-		    *total = 8; // (An,Xn*S)
-		    return true;
-		  }
-		if (REG_P(c) || GET_CODE(c) == SYMBOL_REF || GET_CODE(c) == CONST_INT)
-		  {
-		    *total = 10; // (32,Xn*S)
-		    return true;
-		  }
-	      }
-	    if (GET_CODE(b) == PLUS)
-	      {
-		rtx b0 = XEXP(b, 0);
-		rtx b1 = XEXP(b, 1);
-		if (REG_P(b0) || GET_CODE(b0) == MULT || GET_CODE(b0) == ASHIFT)
-		  {
-		    if (GET_CODE(c) == CONST_INT)
-		      {
-			*total = INTVAL(c) >= -8 && INTVAL(c) <= 7 ? 8 // 8(An,Xn*S)
-			    :
-				 INTVAL(c) >= -32768 && INTVAL(c) <= 32767 ? 9 // (16,An,Xn*S)
-				     : 10; // (32,An,Xn*S)
-			return true;
-		      }
-		    if (GET_CODE(c) == SYMBOL_REF)
-		      {
-			*total = 10;
-			return true;
-		      }
-		  }
-	      }
-	  }
-	*total = 38;
-	return true;
+	// ADDRESS cost + 3
+	bool r = m68k_68080_costs (XEXP(x, 0), mode, ADDRESS, 0, total, speed);
+	*total += 3;
+	return r;
       }
-      break;
 
     case SYMBOL_REF:
     case LABEL_REF:
       *total = 4;
       return true;
 
+    case NE:
+    case EQ:
+    case GE:
+    case GT:
+    case LE:
+    case LT:
+    case GEU:
+    case GTU:
+    case LEU:
+    case LTU:
     case COMPARE:
+      {
+	rtx dst = XEXP(x, 0);
+	rtx src = XEXP(x, 1);
+	bool r = m68k_68080_costs (dst, mode, code, 0, total, speed);
+	total2 = *total;
+	r &= m68k_68080_costs (src, mode, code, 0, total, speed);
+	*total += total2;
+	return r;
+      }
     case SET:
       {
-	rtx a = XEXP(x, 0);
-	rtx b = XEXP(x, 1);
-	bool r = m68k_68080_costs (a, mode, code, 0, total, speed);
-	total2 = *total;
+	total2 = 4;
 
-	if (code == SET)
-	switch (GET_CODE(b))
-	{
-	  case ASHIFT:
-	  case ASHIFTRT:
-	  case LSHIFTRT:
-	  case PLUS:
-	  case MINUS:
-	  case AND:
-	  case IOR:
-	  case XOR:
-	    total2 = 0;
-	    break;
-	  default:
-	    break;
-	}
+	rtx dst = XEXP(x, 0);
+	rtx src = XEXP(x, 1);
+	if (GET_CODE(dst) == PC)
+	  {
+	    if (GET_CODE(src) == IF_THEN_ELSE)
+	      {
+		*total = 16;
+		return true;
+	      }
+	    bool r = m68k_68080_costs (src, mode, ADDRESS, 0, total, speed);
+	    *total += total2;
+	    return true;
+	  }
+	if (GET_CODE(dst) == CC0)
+	  {
+	    return m68k_68080_costs (src, mode, code, 0, total, speed);
+	  }
 
-	r &= m68k_68080_costs (b, mode, code, 0, total, speed);
+	bool r = m68k_68080_costs (dst, mode, code, 0, total, speed);
+	total2 += *total;
+
+	// binops have dst as first argument - check for binop
+	const char *format = GET_RTX_FORMAT(GET_CODE(src));
+	if (format && format[0] == 'e' && format[1] == 'e')
+	  {
+	    if (reload_completed)
+	      {
+		rtx p0 = XEXP(src, 0);
+		if (SUBREG_P(p0))
+		  p0 = XEXP(p0, 0);
+		// reset cost if p0 == dst - do not add cost twice
+		if (GET_CODE(src) == PLUS && !rtx_equal_p (p0, dst))
+		  {
+		    bool r = m68k_68080_costs (src, mode, ADDRESS, 0, total, speed);
+		    *total += total2;
+		    return true;
+		  }
+	      }
+	    // modify dst with dst
+	    total2 = 4; // reset to base cost
+	  }
+
+	r &= m68k_68080_costs (src, mode, code, 0, total, speed);
 	*total += total2;
-
-	if (GET_CODE(b) == COMPARE)
-	  *total -= 2;
-
-	// lea penalty
-	if (REG_P(a) && GET_CODE(b) == PLUS && XEXP(b, 0) != a)
-	  *total += 1;
-
-
 
 	return r;
       }
